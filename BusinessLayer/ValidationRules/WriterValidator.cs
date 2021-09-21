ed32a4f8cfc8eb2ad0231cd3223e08f5c7f2e2e4
sueda akın <sueda_akin@hotmail.com>
2021-09-21 23:01:54 +0300

    50. ders

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,32 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Ünvan kısmını boş geçemezsiniz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında kısmını boş geçemezsiniz");
+            RuleFor(x => x.WriterAbout).Must(AboutValid).WithMessage("A harfini içermelidir");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az üç karakter girişi yapın");
+            RuleFor(x => x.WriterSurName).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayın");
+        }
+
+        private bool AboutValid(string arg)
+        {
+            Regex regex = new Regex(@"a");
+            return regex.IsMatch(arg);
+        }
+    }
+}
+
---
+++ DataAccessLayer/Migrations/202109211324575_mig_writer_edit1.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_writer_edit1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Writers", "WriterAbout", c => c.String(maxLength: 100));
+            DropColumn("dbo.Writers", "About");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Writers", "About", c => c.String(maxLength: 100));
+            DropColumn("dbo.Writers", "WriterAbout");
+        }
+    }
+}
